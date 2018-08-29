767af4e2e82a7a698a2a1de98b1fd359cd4cce92
Will Adler <will@wtadler.com>
2018-08-29 13:43:37 -0400

    clean up map code

--- Maps/Interactive/make_html_map.py
+++ Maps/Interactive/make_html_map.py
@@ -5,9 +5,8 @@ import numpy as np
 import shapely.ops as so
 import random
 
+# get spaced out colors, shuffle them
 import matplotlib
-from pylab import cm
-
 cmap = matplotlib.cm.get_cmap('gist_rainbow')
 colors = cmap(np.linspace(0,1,33))
 np.random.seed(105)
@@ -17,20 +16,28 @@ affected = [63, 69, 70, 71, 74, 77, 80, 89, 90, 92, 95]
 adjacent= [27, 55, 61, 62, 64, 66, 68, 72, 73, 75, 76, 78, 79, 81, 83, 85, 91, 93, 94, 96, 97, 100]
 bh = affected + adjacent
 
+bounds = [[36.482, -78.91], [38.22,-75.19]]
+
+################## #
+# load enacted map #
+################## #
 enacted_map = '../Enacted map/enacted.shp'
 df_enacted = gpd.read_file(enacted_map)
 
 # turn non-BH districts into a single shape
 nonBH = so.cascaded_union(df_enacted.loc[~df_enacted['ID'].isin(bh), 'geometry'])
 
-bounds = [[36.482, -78.91], [38.22,-75.19]]
-
-# filter
+# filter to just relevant districts
 df_enacted = df_enacted[df_enacted['ID'].isin(bh)]
 
 # some stuff for labeling
 df_enacted['Map'] = 'Enacted map'
 df_enacted['Empty'] = ''
+
+
+###################
+# load reform map #
+###################
 
 reform_map = '../Reform map/Districts map bethune-hill final.shp'
 df_reform = gpd.read_file(reform_map)[1:]
@@ -42,30 +49,35 @@ df_reform['Empty'] = ''
 
 
 # assign colors as shuffled above
+def rgb_to_hex(rgb):
+    f = lambda x: int(x*255)
+    return '#%02X%02X%02X' % (f(rgb[0]), f(rgb[1]), f(rgb[2]))
+    
 for i, district in enumerate(bh):
-    color = '#%02X%02X%02X' % (int(colors[i][0]*255),int(colors[i][1]*255),int(colors[i][2]*255))
+    color = rgb_to_hex(colors[i])
+    # color = '#%02X%02X%02X' % (int(colors[i][0]*255),int(colors[i][1]*255),int(colors[i][2]*255))
     df_enacted.loc[df_enacted['ID']==district, 'color'] = color
     df_reform.loc[df_reform['DISTRICT'].astype(int)==district, 'color'] = color
-
-
-
-# r = lambda: np.random.randint(255)
-# random_color = lambda: '#%02X%02X%02X' % (r(),r(),r())
 
 style_function = lambda x: {'fillColor': x['properties']['color'] if 'color' in x['properties'] else '#fff',
                             'fillOpacity': 0.35,
                             'weight': 1,
                             'color': '#888'}
 
+
+###################
+# make folium map #
+###################
+
 m = folium.Map(tiles=None, control_scale=True, min_lat=bounds[0][0], max_lat=bounds[1][0], min_lon=bounds[0][1], max_lon=bounds[1][1])
 
 tooltip_reform = folium.features.GeoJsonTooltip(['Empty', 'DISTRICT'], aliases=['Reform map', 'District'])
-
 folium.features.GeoJson(df_reform, name='Reform map', style_function=style_function, show=True, tooltip=tooltip_reform).add_to(m)
 
 tooltip_enacted = folium.features.GeoJsonTooltip(['Empty', 'ID'], aliases=['Enacted map', 'District'])
 folium.features.GeoJson(df_enacted, name='Enacted map', style_function=style_function, show=False, tooltip=tooltip_enacted).add_to(m)
 
+# non-relevant VA districts
 folium.features.GeoJson(nonBH, show=True, control=False, style_function=lambda x: {'fillColor': '#000', 'weight': 0, 'fillOpacity': .5}, name='nonBH districts', tooltip='Non-affected districts').add_to(m)
 
 folium.raster_layers.TileLayer(control=False, min_zoom=8).add_to(m)
