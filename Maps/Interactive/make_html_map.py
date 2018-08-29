7447008061c52d3d887c1eb3e4affd529ff1f2a4
Will Adler <will@wtadler.com>
2018-08-29 02:33:40 -0400

    move map, link from readme

---
+++ Maps/Interactive/make_html_map.py
@@ -0,0 +1,55 @@
+import folium
+import geopandas as gpd
+%matplotlib inline
+import numpy as np
+import shapely.ops as so
+
+affected = [63, 69, 70, 71, 74, 77, 80, 89, 90, 92, 95]
+adjacent= [27, 55, 61, 62, 64, 66, 68, 72, 73, 75, 76, 78, 79, 81, 83, 85, 91, 93, 94, 96, 97, 100]
+bh = affected + adjacent
+
+enacted_map = 'Enacted map/enacted.shp'
+df_enacted = gpd.read_file(enacted_map)
+
+# turn non-BH districts into a single shape
+nonBH = so.cascaded_union(df_enacted.loc[~df_enacted['ID'].isin(bh), 'geometry'])
+
+# filter
+df_enacted = df_enacted[df_enacted['ID'].isin(bh)]
+
+# some stuff for labeling
+df_enacted['Map'] = 'Enacted map'
+df_enacted['Empty'] = ''
+
+reform_map = 'Reform map/Districts map bethune-hill final.shp'
+df_reform = gpd.read_file(reform_map)[1:]
+
+# some stuff for labeling
+df_reform['Map'] = 'Reform map'
+df_reform['Empty'] = ''
+
+r = lambda: np.random.randint(255)
+random_color = lambda: '#%02X%02X%02X' % (r(),r(),r())
+
+style_function = lambda x: {'fillColor': random_color(),
+                            'fillOpacity': 0.35,
+                            'weight': 1,
+                            'color': '#000'}
+
+m = folium.Map(tiles='openstreetmap', control_scale=True, min_zoom=8)
+
+tooltip_reform = folium.features.GeoJsonTooltip(['Empty'], aliases=['Reform map'])
+
+folium.features.GeoJson(df_reform, name='Reform map', style_function=style_function, show=True, tooltip=tooltip_reform).add_to(m)
+
+tooltip_enacted = folium.features.GeoJsonTooltip(['Empty', 'ID'], aliases=['Enacted map', 'District'])
+folium.features.GeoJson(df_enacted, name='Enacted map', style_function=style_function, show=False, tooltip=tooltip('Enacted map')).add_to(m)
+
+folium.features.GeoJson(nonBH, show=True, control=False, style_function=lambda x: {'fillColor': '#000', 'weight': 0, 'fillOpacity': .5}, name='nonBH districts', tooltip='Non-affected districts').add_to(m)
+
+folium.LayerControl(collapsed=False).add_to(m)
+
+folium.map.FitBounds([[36.482, -78.91], [38.22,-75.19]]).add_to(m)
+
+m.save('map_comparison.html')
+
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## Accompanying data and analysis for "A Fresh Start for Virginia Districting"
 ### Ben Williams, William T. Adler, and Samuel S.-H. Wang, of the [Princeton Gerrymandering Project](http://gerrymander.princeton.edu/)
 ### Additional work by Connor Moffatt and Jacob Wachspress
-<p align="center"><img src="https://raw.githubusercontent.com/PrincetonUniversity/VA-gerrymander/master/Maps/Reform%20map/stacked_richmondinset.jpg" width="400"></p>
-<p align="center">Click to enlarge, or <a href="http://wtadler.com/BH_VA_maps.html">go here</a> to explore the maps interactively.</p>
+<p align="center"><a href="https://cdn.rawgit.com/PrincetonUniversity/VA-gerrymander/9ac12264/Maps/map_comparison.html"><img src="Maps/interactive/map_preview.png" width="700"></a></p>
+<p align="center">Click to explore the maps interactively.</p>
 
 This repository contains a Reform Map drawn by the Princeton Gerrymandering Project to remedy the unconstitutional districts recently struck down as racial gerrymanders in Bethune-Hill v. Va. State Bd. of Elections (opinion [here](http://electionlawblog.org/wp-content/uploads/Virginia.pdf)). 
 On August 30, 2018, the General Assembly will enter a special session to consider proposals from both political parties and the public on how the afflicted districts should be remedied.
