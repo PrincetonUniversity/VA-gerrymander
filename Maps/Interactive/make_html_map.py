ad6ee28f5c4863f4284c105aae91fbdb4c2ada17
Will Adler <will@wtadler.com>
2018-08-29 08:46:51 -0400

    add district numbers to reform map, mobile layout

--- Maps/Interactive/make_html_map.py
+++ Maps/Interactive/make_html_map.py
@@ -8,7 +8,7 @@ affected = [63, 69, 70, 71, 74, 77, 80, 89, 90, 92, 95]
 adjacent= [27, 55, 61, 62, 64, 66, 68, 72, 73, 75, 76, 78, 79, 81, 83, 85, 91, 93, 94, 96, 97, 100]
 bh = affected + adjacent
 
-enacted_map = 'Enacted map/enacted.shp'
+enacted_map = '../Enacted map/enacted.shp'
 df_enacted = gpd.read_file(enacted_map)
 
 # turn non-BH districts into a single shape
@@ -21,7 +21,7 @@ df_enacted = df_enacted[df_enacted['ID'].isin(bh)]
 df_enacted['Map'] = 'Enacted map'
 df_enacted['Empty'] = ''
 
-reform_map = 'Reform map/Districts map bethune-hill final.shp'
+reform_map = '../Reform map/Districts map bethune-hill final.shp'
 df_reform = gpd.read_file(reform_map)[1:]
 
 # some stuff for labeling
@@ -34,22 +34,27 @@ random_color = lambda: '#%02X%02X%02X' % (r(),r(),r())
 style_function = lambda x: {'fillColor': random_color(),
                             'fillOpacity': 0.35,
                             'weight': 1,
-                            'color': '#000'}
+                            'color': '#888'}
 
 m = folium.Map(tiles='openstreetmap', control_scale=True, min_zoom=8)
 
-tooltip_reform = folium.features.GeoJsonTooltip(['Empty'], aliases=['Reform map'])
+tooltip_reform = folium.features.GeoJsonTooltip(['Empty', 'District'], aliases=['Reform map', 'District'])
 
 folium.features.GeoJson(df_reform, name='Reform map', style_function=style_function, show=True, tooltip=tooltip_reform).add_to(m)
 
 tooltip_enacted = folium.features.GeoJsonTooltip(['Empty', 'ID'], aliases=['Enacted map', 'District'])
-folium.features.GeoJson(df_enacted, name='Enacted map', style_function=style_function, show=False, tooltip=tooltip('Enacted map')).add_to(m)
+folium.features.GeoJson(df_enacted, name='Enacted map', style_function=style_function, show=False, tooltip=tooltip_enacted).add_to(m)
 
 folium.features.GeoJson(nonBH, show=True, control=False, style_function=lambda x: {'fillColor': '#000', 'weight': 0, 'fillOpacity': .5}, name='nonBH districts', tooltip='Non-affected districts').add_to(m)
 
 folium.LayerControl(collapsed=False).add_to(m)
 
 folium.map.FitBounds([[36.482, -78.91], [38.22,-75.19]]).add_to(m)
+
+m.get_root().header.add_child(folium.Element(
+    '<meta name="viewport" content="width=device-width,'
+    ' initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />'
+))
 
 m.save('map_comparison.html')
 
