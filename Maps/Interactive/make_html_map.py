dd97d6257282a631af31f439f29a33a57a043825
Will Adler <will@wtadler.com>
2018-08-29 13:22:30 -0400

    separate colors, fix across both maps

--- Maps/Interactive/make_html_map.py
+++ Maps/Interactive/make_html_map.py
@@ -3,8 +3,15 @@ import geopandas as gpd
 %matplotlib inline
 import numpy as np
 import shapely.ops as so
+import random
 
-np.random.seed(100)
+import matplotlib
+from pylab import cm
+
+cmap = matplotlib.cm.get_cmap('gist_rainbow')
+colors = cmap(np.linspace(0,1,33))
+np.random.seed(105)
+np.random.shuffle(colors)
 
 affected = [63, 69, 70, 71, 74, 77, 80, 89, 90, 92, 95]
 adjacent= [27, 55, 61, 62, 64, 66, 68, 72, 73, 75, 76, 78, 79, 81, 83, 85, 91, 93, 94, 96, 97, 100]
@@ -27,20 +34,30 @@ df_enacted['Empty'] = ''
 
 reform_map = '../Reform map/Districts map bethune-hill final.shp'
 df_reform = gpd.read_file(reform_map)[1:]
+df_reform['DISTRICT'] = df_reform['DISTRICT'].astype(str)
 
 # some stuff for labeling
 df_reform['Map'] = 'Reform map'
 df_reform['Empty'] = ''
 
-r = lambda: np.random.randint(255)
-random_color = lambda: '#%02X%02X%02X' % (r(),r(),r())
 
-style_function = lambda x: {'fillColor': random_color(),
+# assign colors as shuffled above
+for i, district in enumerate(bh):
+    color = '#%02X%02X%02X' % (int(colors[i][0]*255),int(colors[i][1]*255),int(colors[i][2]*255))
+    df_enacted.loc[df_enacted['ID']==district, 'color'] = color
+    df_reform.loc[df_reform['DISTRICT'].astype(int)==district, 'color'] = color
+
+
+
+# r = lambda: np.random.randint(255)
+# random_color = lambda: '#%02X%02X%02X' % (r(),r(),r())
+
+style_function = lambda x: {'fillColor': x['properties']['color'] if 'color' in x['properties'] else '#fff',
                             'fillOpacity': 0.35,
                             'weight': 1,
                             'color': '#888'}
 
-m = folium.Map(tiles=None, control_scale=True) #min_lat=bounds[0][0], max_lat=bounds[1][0], min_lon=bounds[0][1], max_lon=bounds[1][1])
+m = folium.Map(tiles=None, control_scale=True, min_lat=bounds[0][0], max_lat=bounds[1][0], min_lon=bounds[0][1], max_lon=bounds[1][1])
 
 tooltip_reform = folium.features.GeoJsonTooltip(['Empty', 'DISTRICT'], aliases=['Reform map', 'District'])
 
@@ -65,8 +82,6 @@ info_box = '''
     '''
     
 m.get_root().html.add_child(folium.Element(info_box))
-
-
 
 folium.map.FitBounds(bounds).add_to(m)
 
