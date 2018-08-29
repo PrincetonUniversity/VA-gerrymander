ceb1eb0035c5e4b683efddd26d1d039200aa571d
Will Adler <will@wtadler.com>
2018-08-29 10:27:30 -0400

    small map improvements

--- Maps/Interactive/make_html_map.py
+++ Maps/Interactive/make_html_map.py
@@ -16,6 +16,8 @@ df_enacted = gpd.read_file(enacted_map)
 # turn non-BH districts into a single shape
 nonBH = so.cascaded_union(df_enacted.loc[~df_enacted['ID'].isin(bh), 'geometry'])
 
+bounds = [[36.482, -78.91], [38.22,-75.19]]
+
 # filter
 df_enacted = df_enacted[df_enacted['ID'].isin(bh)]
 
@@ -38,7 +40,7 @@ style_function = lambda x: {'fillColor': random_color(),
                             'weight': 1,
                             'color': '#888'}
 
-m = folium.Map(tiles='openstreetmap', control_scale=True, min_zoom=8)
+m = folium.Map(tiles=None, control_scale=True) #min_lat=bounds[0][0], max_lat=bounds[1][0], min_lon=bounds[0][1], max_lon=bounds[1][1])
 
 tooltip_reform = folium.features.GeoJsonTooltip(['Empty', 'DISTRICT'], aliases=['Reform map', 'District'])
 
@@ -49,9 +51,24 @@ folium.features.GeoJson(df_enacted, name='Enacted map', style_function=style_fun
 
 folium.features.GeoJson(nonBH, show=True, control=False, style_function=lambda x: {'fillColor': '#000', 'weight': 0, 'fillOpacity': .5}, name='nonBH districts', tooltip='Non-affected districts').add_to(m)
 
+folium.raster_layers.TileLayer(control=False, min_zoom=8).add_to(m)
+
 folium.LayerControl(collapsed=False).add_to(m)
 
-folium.map.FitBounds([[36.482, -78.91], [38.22,-75.19]]).add_to(m)
+info_box = '''
+     <div style="position: fixed; top: 12px; left: 70px; border: 0px; z-index: 9999; font-size: 13px; border-radius: 5px; background-color: #fff; padding: 8px; box-shadow: 0px 2px 4px #888; opacity: 0.85;">
+     <b style="font-size: 16px">Accompanying maps for "A Fresh Start for Virginia Districting"</b><br>
+     Ben Williams, William T. Adler, and Samuel S.-H. Wang of the <a style="color: #e77500" href="http://gerrymander.princeton.edu/">Princeton Gerrymandering Project</a><br>
+     Additional work by Connor Moffatt and Jacob Wachspress<br>
+     More info <a style="color: #e77500" href="https://github.com/PrincetonUniversity/VA-gerrymander">here</a>
+      </div>
+    '''
+    
+m.get_root().html.add_child(folium.Element(info_box))
+
+
+
+folium.map.FitBounds(bounds).add_to(m)
 
 m.get_root().header.add_child(folium.Element(
     '<meta name="viewport" content="width=device-width,'
