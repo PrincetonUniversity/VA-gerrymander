60fea50031cdc5b792ba27581141da24af5fd577
Will Adler <will@wtadler.com>
2018-08-30 11:39:24 -0400

    improve color separation, mark affected v. adjacent

--- Maps/Interactive/make_html_map.py
+++ Maps/Interactive/make_html_map.py
@@ -4,17 +4,42 @@ import numpy as np
 import shapely
 import fileinput
 import matplotlib.cm as cm
+import pandas as pd
 
+
+#%%
 # get spaced out colors, shuffle them
 cmap = cm.get_cmap('gist_rainbow')
 colors = cmap(np.linspace(0,1,33))
 np.random.seed(105)
 np.random.shuffle(colors)
 
+def rgb_to_hex(rgb):
+    f = lambda x: int(x*255)
+    return '#%02X%02X%02X' % (f(rgb[0]), f(rgb[1]), f(rgb[2]))
+
 # identify relevant districts
 affected = [63, 69, 70, 71, 74, 77, 80, 89, 90, 92, 95]
 adjacent= [27, 55, 61, 62, 64, 66, 68, 72, 73, 75, 76, 78, 79, 81, 83, 85, 91, 93, 94, 96, 97, 100]
-bh = [str(i) for i in affected + adjacent]
+# bh = [str(i) for i in affected + adjacent]
+colordict = {}
+affected_label = 'Ruled unconstitutional as enacted'
+adjacent_label = 'Adjacent to a district ruled unconstitutional'
+for i, district in enumerate(affected):
+    colordict[str(district)] = {'status': affected_label,
+                                'color': rgb_to_hex(colors[i])}
+for i, district in enumerate(adjacent):
+    colordict[str(district)] = {'status': adjacent_label,
+                                'color': rgb_to_hex(colors[i+len(affected)])}
+    
+# manually change some colors:
+colordict['62']['color'] = '#002235'
+colordict['83']['color'] = colordict['95']['color']
+colordict['81']['color'] = '#330035'
+colordict['64']['color'] = colordict['61']['color']
+colordict['97']['color'] = colordict['75']['color']
+
+color_df = pd.DataFrame.from_dict(colordict, orient='index')
 
 # map boundaries, SW and NE points
 bounds = [[36.482, -78.91], [38.22,-75.19]]
@@ -34,9 +59,7 @@ maps = {'reform': {'name': 'Reform map',
                     'show': False}
         }
 
-def rgb_to_hex(rgb):
-    f = lambda x: int(x*255)
-    return '#%02X%02X%02X' % (f(rgb[0]), f(rgb[1]), f(rgb[2]))
+common_colname = 'district_no'
 
 for mapname in maps:
     df = gpd.read_file(maps[mapname]['path'])
@@ -44,14 +67,13 @@ for mapname in maps:
     if mapname=='enacted':
         nonBH = shapely.ops.cascaded_union(df.loc[~df[maps['enacted']['district_colname']].isin(bh), 'geometry'])
     
-    df[maps[mapname]['district_colname']] = df[maps[mapname]['district_colname']].astype(str)
-    df = df[df[maps[mapname]['district_colname']].isin(bh)]
+    df = df.rename(columns={maps[mapname]['district_colname']: common_colname})
+    df[common_colname] = df[common_colname].astype(str)
+    df = df[df[common_colname].isin(bh)]
     
     # assign colors as shuffled
-    for i, district in enumerate(bh):
-        color = rgb_to_hex(colors[i])
-        df.loc[df[maps[mapname]['district_colname']]==district, 'color'] = color
-
+    df = df.merge(color_df, left_on=common_colname, right_index=True)
+    
     # for labeling purposes
     df['Empty'] = ''
     
@@ -59,15 +81,15 @@ for mapname in maps:
 
 # polygon style
 style_function = lambda x: {'fillColor': x['properties']['color'] if 'color' in x['properties'] else '#fff',
-                            'fillOpacity': 0.35,
-                            'weight': 1.4,
+                            'fillOpacity': 0.2 if x['properties']['status']==adjacent_label else 0.58,
+                            'weight': 1.5 if x['properties']['status']==adjacent_label else 3.2,
                             'color': '#888'}
 
 # style when mousing over
 highlight_function = lambda x: {'fillColor': x['properties']['color'] if 'color' in x['properties'] else '#fff',
-                            'fillOpacity': 0.65,
-                            'weight': 1.4,
-                            'color': '#888'}
+                                'fillOpacity': 0.65,
+                                'weight': 1.5 if x['properties']['status']==adjacent_label else 3.2,
+                                'color': '#888'}
 
 
 ###################
@@ -77,8 +99,8 @@ highlight_function = lambda x: {'fillColor': x['properties']['color'] if 'color'
 m = folium.Map(tiles=None, control_scale=True, min_lat=bounds[0][0], max_lat=bounds[1][0], min_lon=bounds[0][1], max_lon=bounds[1][1], max_bounds=True)
 
 for mapname in maps:
-    tooltip = folium.features.GeoJsonTooltip(['Empty', maps[mapname]['district_colname']],
-                                             aliases=[maps[mapname]['name'], 'District'])
+    tooltip = folium.features.GeoJsonTooltip(['Empty', 'status', common_colname],
+                                             aliases=[maps[mapname]['name'], 'Status', 'District'])
     folium.features.GeoJson(maps[mapname]['df'],
                             name=maps[mapname]['name'],
                             style_function=style_function,
