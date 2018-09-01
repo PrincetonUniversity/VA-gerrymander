7158e2696d9b9ca7f740bc973ba6785c3df4b859
Connor <connormoffatt4@gmail.com>
2018-09-01 08:05:26 -0400

    Adding comments

--- Maps/Interactive/make_html_map.py
+++ Maps/Interactive/make_html_map.py
@@ -8,20 +8,29 @@ import pandas as pd
 
 
 #%%
-# get spaced out colors, shuffle them
-cmap = cm.get_cmap('gist_rainbow')
-colors = cmap(np.linspace(0,1,33))
-np.random.seed(105)
-np.random.shuffle(colors)
 
+# Color conversion helper function
 def rgb_to_hex(rgb):
     f = lambda x: int(x*255)
     return '#%02X%02X%02X' % (f(rgb[0]), f(rgb[1]), f(rgb[2]))
 
+# set the color map
+cmap = cm.get_cmap('gist_rainbow')
+
+# create 33 colors and shuffle
+colors = cmap(np.linspace(0,1,33))
+np.random.seed(105)
+np.random.shuffle(colors)
+
+
+
 # identify relevant districts
 affected = [63, 69, 70, 71, 74, 77, 80, 89, 90, 92, 95]
-adjacent= [27, 55, 61, 62, 64, 66, 68, 72, 73, 75, 76, 78, 79, 81, 83, 85, 91, 93, 94, 96, 97, 100]
+adjacent= [27, 55, 61, 62, 64, 66, 68, 72, 73, 75, 76, 78, 79, 81, 83, 85, \
+           91, 93, 94, 96, 97, 100]
 bh = [str(i) for i in affected + adjacent]
+
+# Set colors for each dictionary
 colordict = {}
 affected_label = 'Ruled unconstitutional as enacted'
 adjacent_label = 'Adjacent to a district ruled unconstitutional'
@@ -32,29 +41,38 @@ for i, district in enumerate(adjacent):
     colordict[str(district)] = {'status': adjacent_label,
                                 'color': rgb_to_hex(colors[i+len(affected)])}
     
-# manually change some colors:
+# manually adjust colors:
 colordict['62']['color'] = '#002235'
 colordict['83']['color'] = colordict['95']['color']
 colordict['81']['color'] = '#330035'
 colordict['64']['color'] = colordict['61']['color']
 colordict['97']['color'] = colordict['75']['color']
 
+# Create dataframe from the color dictionary
 color_df = pd.DataFrame.from_dict(colordict, orient='index')
 
 # map boundaries, SW and NE points
 bounds = [[36.482, -78.91], [38.22,-75.19]]
 
-# info on what maps to load
+# Dictionary of dictionary containing information about every option on the
+# interactive map
+    # name: name of map in selection map
+    # path: Path after /Interactive
+    # district_colname: name of column that refers to a given district
+    # show: ???
+    
+start_path = 'C:/Users/conno/Documents/GitHub/VA-gerrymander/'
 maps = {'reform': {'name': 'Reform map',
-                   'path': 'Maps/Reform map/Districts map bethune-hill final.shp',
+                   'path': start_path + 'Maps/Reform map/Districts map ' + \
+                           'bethune-hill final.shp',
                    'district_colname': 'DISTRICT',
                    'show': True},
         'enacted': {'name': 'Enacted map',
-                    'path': 'Maps/Enacted map/enacted.shp',
+                    'path': start_path + 'Maps/Enacted map/enacted.shp',
                     'district_colname': 'ID',
                     'show': False},
         'dems':    {'name': 'VA House Dems map',
-                    'path': 'Maps/House Dems map/HB7001.shp',
+                    'path': start_path + 'Maps/House Dems map/HB7001.shp',
                     'district_colname': 'OBJECTID',
                     'show': False}
         }
@@ -67,11 +85,15 @@ replace the district_colname key with 'bvap_prop', and the value indicates the c
 
 common_colname = 'district_no'
 
+#Iterate through every option on the interactive map
 for mapname in maps:
     df = gpd.read_file(maps[mapname]['path'])
     
     if mapname=='enacted':
-        nonBH = shapely.ops.cascaded_union(df.loc[~df[maps['enacted']['district_colname']].isin(bh), 'geometry'])
+        nonBH = shapely.ops.cascaded_union(df.loc[\
+                                                  ~df[maps['enacted']\
+                                                      ['district_colname']]\
+                                                      .isin(bh), 'geometry'])
     
     df = df.rename(columns={maps[mapname]['district_colname']: common_colname})
     df[common_colname] = df[common_colname].astype(str)
@@ -89,17 +111,21 @@ for mapname in maps:
 make a loop just like the above that loops over all the choropleths you might want
 '''
 
-
 # polygon style
-style_function = lambda x: {'fillColor': x['properties']['color'] if 'color' in x['properties'] else '#fff',
-                            'fillOpacity': 0.2 if x['properties']['status']==adjacent_label else 0.58,
-                            'weight': 1.5 if x['properties']['status']==adjacent_label else 3.2,
+style_function = lambda x: {'fillColor': x['properties']['color'] if 'color'\
+                                            in x['properties'] else '#fff',
+                            'fillOpacity': 0.2 if x['properties']['status']==\
+                                                adjacent_label else 0.58,
+                            'weight': 1.5 if x['properties']['status']==\
+                                            adjacent_label else 3.2,
                             'color': '#888'}
 
 # style when mousing over
-highlight_function = lambda x: {'fillColor': x['properties']['color'] if 'color' in x['properties'] else '#fff',
+highlight_function = lambda x: {'fillColor': x['properties']['color'] if \
+                                        'color' in x['properties'] else '#fff',
                                 'fillOpacity': 0.65,
-                                'weight': 1.5 if x['properties']['status']==adjacent_label else 3.2,
+                                'weight': 1.5 if x['properties']['status']==\
+                                                    adjacent_label else 3.2,
                                 'color': '#888'}
 
 ''' CHOROPLETHS TODO
@@ -114,11 +140,15 @@ then make a style_function_choropleth like this, except with fillcolor being som
 # make folium map #
 ###################
 
-m = folium.Map(tiles=None, control_scale=True, min_lat=bounds[0][0], max_lat=bounds[1][0], min_lon=bounds[0][1], max_lon=bounds[1][1], max_bounds=True)
+m = folium.Map(tiles=None, control_scale=True, min_lat=bounds[0][0], \
+               max_lat=bounds[1][0], min_lon=bounds[0][1], \
+               max_lon=bounds[1][1], max_bounds=True)
 
 for mapname in maps:
-    tooltip = folium.features.GeoJsonTooltip(['Empty', 'status', common_colname],
-                                             aliases=[maps[mapname]['name'], 'Status', 'District'])
+    tooltip = folium.features.GeoJsonTooltip(['Empty', 'status', \
+                                              common_colname],\
+                                             aliases=[maps[mapname]['name'],\
+                                                      'Status', 'District'])
     folium.features.GeoJson(maps[mapname]['df'],
                             name=maps[mapname]['name'],
                             style_function=style_function,
@@ -136,7 +166,11 @@ Then add a colorbar that is visible whenever this layer is visible. Not sure how
 
 
 # non-relevant VA districts
-folium.features.GeoJson(nonBH, show=True, control=False, style_function=lambda x: {'fillColor': '#000', 'weight': 0, 'fillOpacity': .5}, name='nonBH districts', tooltip='Non-affected districts').add_to(m)
+folium.features.GeoJson(nonBH, show=True, control=False, \
+                        style_function=lambda x: \
+                        {'fillColor': '#000', 'weight': 0, 'fillOpacity': .5},\
+                         name='nonBH districts', \
+                         tooltip='Non-affected districts').add_to(m)
 
 folium.raster_layers.TileLayer(control=False, min_zoom=8).add_to(m)
 
@@ -160,6 +194,6 @@ m.get_root().header.add_child(folium.Element(
     ' initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />'
 ))
 
-filename = 'Maps/interactive/map_comparison.html'
+filename = "C:/Users/conno/Documents/GitHub/VA-gerrymander/Maps/Interactive/map_comparison.html"
 m.save(filename)
 
