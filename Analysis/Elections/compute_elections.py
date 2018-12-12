822b00c8065f01cd48419d9dbc90417a88ec94ec
hwheelen <hwheelen@berkeley.edu>
2018-12-12 15:50:08 -0500

    elections analysis

--- Analysis/Elections/compute_elections.py
+++ Analysis/Elections/compute_elections.py
@@ -1,42 +1,60 @@
 import geopandas as gpd
 import pandas as pd
 import sys
-sys.path.append('/Users/wtadler/Repos/gerrymander-geoprocessing/areal_interpolation')
-import areal_interpolation as ai
+sys.path.append('/gerrymander-geoprocessing/areal_interpolation')
+from areal_interpolation import areal_interpolation as ai
 import tabulate
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
-%config InlineBackend.figure_format = 'svg'
+#%config InlineBackend.figure_format = 'svg'
 
 unconstitutional_only = False
 
+start_path = ''
+
 maps = {'reform': {'name': 'PGP Reform map',
-                   'path': 'Maps/Reform map/Districts map bethune-hill final.shp',
-                   'district_colname': 'DISTRICT',
-                   'show': True,
-                   'color': 'orange'},
+                   'path': start_path + 'Maps/Reform map/Districts map bethune-hill final.shp',
+                   'district_colname': 'DISTRICT',  'color': 'blue',
+                   'show': True},
         'enacted': {'name': 'Enacted map',
-                    'path': 'Maps/Enacted map/enacted.shp',
-                    'district_colname': 'ID',
-                    'show': False,
-                    'color': 'violet'},
+                    'path': start_path + 'Maps/Enacted map/enacted.shp',
+                    'district_colname': 'ID',  'color': 'green',
+                    'show': False},
         'dems':    {'name': 'VA House Dems map',
-                    'path': 'Maps/House Dems map/HB7001.shp',
-                    'district_colname': 'OBJECTID',
-                    'show': False,
-                    'color': 'blue'},
-        'gop':     {'name': 'VA House GOP Map', 
-                    'path': 'Maps/GOP map bell substitute/HB7002_ANS.shp',
-                    'district_colname': 'OBJECTID',
-                    'show': False,
-                    'color': 'red'},
-        'new_VA':  {'name': 'New VA Majority',
-                    'path': 'Maps/New VA Majority/VA NVM Map Submission 20180926.shp',
-                    'district_colname': 'District',
-                    'show': False,
-                    'color': 'green'}
+                    'path': start_path + 'Maps/House Dems map/HB7001.shp',
+                    'district_colname': 'OBJECTID',  'color': 'red',
+                    'show': False},
+# =============================================================================
+#         'gop_bell1':     {'name': 'VA House GOP (Bell 1)',
+#                     'path': start_path + 'Maps/GOP map bell/HB7002_shapefile.shp',
+#                     'district_colname': 'OBJECTID',
+#                     'show': False},
+# =============================================================================
+# =============================================================================
+#         'gop_bell2':     {'name': 'VA House GOP (Bell)',
+#                     'path': start_path + 'Maps/GOP map bell substitute/HB7002_ANS.shp',
+#                     'district_colname': 'OBJECTID',  'color': 4,
+#                     'show': False}, 
+# =============================================================================
+        'gop_jones':    {'name': 'VA House GOP (Jones)',
+                    'path': start_path + 'Maps/GOP map jones/HB7003.shp',
+                    'district_colname': 'OBJECTID',  'color': 'cyan',
+                    'show': False},
+        'new_VA':    {'name': 'New VA Majority',
+                    'path': start_path + 'Maps/New VA Majority/VA NVM Map Submission 20180926.shp',
+                    'district_colname': 'District',  'color': 'violet',
+                    'show': False},
+        'SM_few_changes': {'name': 'Special Master (fewest changes from Enacted)',
+                    'path':start_path + '/Maps/Special Master Map/fewest changes/FewestChangesSM.shp',
+                    'district_colname': 'District_N',  'color': 'magenta',
+                    'show': False},
+        'SM_most_changes': {'name': 'Special Master (most changes from Enacted)',
+                    'path':start_path + '/Maps/Special Master Map/most changes/MostChangesSM.shp',
+                    'district_colname': 'District_N',  'color': 'yellow',
+                    'show': False}
         }
+        
 
 common_colname = 'district_no'
 
@@ -54,28 +72,27 @@ adjacent_label = 'Adjacent to a district ruled unconstitutional'
 
 #%%
 # get census block geography with BVAP and VAP data
-precincts = 'Maps/Relevant precincts/BH_precincts_with_BVAP_VAP.shp'
+precincts = '/Maps/Relevant precincts/BH_precincts_with_BVAP_VAP.shp'
 precincts = gpd.read_file(precincts)
 
 potential_cols = ['locality', 'precinct', 'NAME', 'BVAP', 'VAP', 'prop_BVAP', 'prop_D_LG', 'prop_D_p', 'prop_D_G', 'prop_D_AG', 'prop_D_P', 'index', 'geometry']
 
 vote_cols = [i for i in precincts.columns if not any([i==j for j in potential_cols])]
-
 for mapname in maps:
-    df = gpd.read_file(maps[mapname]['path'])
-    
-    df = df.rename(columns={maps[mapname]['district_colname']: common_colname})
+        df = gpd.read_file(maps[mapname]['path'])
         
-    df[common_colname] = df[common_colname].astype(str)
-    df = df[df[common_colname].isin(bh)]
+        df = df.rename(columns={maps[mapname]['district_colname']: common_colname})
+            
+        df[common_colname] = df[common_colname].astype(str)
+        df = df[df[common_colname].isin(bh)]
+        
+        df = ai.aggregate(precincts, df, source_columns=vote_cols, method='fractional_area')[1]
     
-    df = ai.aggregate(precincts, df, source_columns=vote_cols, method='fractional_area')[1]
-
-    df.loc[df[common_colname].isin([str(i) for i in affected]), 'status'] = affected_label
-    df.loc[df[common_colname].isin([str(i) for i in adjacent]), 'status'] = adjacent_label
-
-    maps[mapname]['df'] = df
-
+        df.loc[df[common_colname].isin([str(i) for i in affected]), 'status'] = affected_label
+        df.loc[df[common_colname].isin([str(i) for i in adjacent]), 'status'] = adjacent_label
+    
+        maps[mapname]['df'] = df
+    
 pres16 = {'Clinton v. Trump (2016)': ['P_DEM_16_x', 'P_REP_16_x']}
 other_elections = {'Clinton v. Sanders (2016)': ['P_HC_16_x', 'P_BS_16_x'],
              'Northam v. Gillespie (2017)': ['G_DEM_17_x', 'G_REP_17_x'],
@@ -87,7 +104,7 @@ all_elex = {**pres16, **other_elections}
 #%%
 figs = {'election_results': {'maps': [i for i in maps],
                              'elections': all_elex},
-        'election_results_pres_only': {'maps': ['reform', 'dems', 'gop', 'new_VA'],
+        'election_results_pres_only': {'maps': ['reform', 'enacted', 'SM_most_changes','SM_few_changes' ],
                                        'elections': pres16}}
 
 for f in figs:
@@ -119,6 +136,5 @@ for f in figs:
         ax[0].legend(loc='upper left')
         ax[0].set_ylabel('Candidate 1 voteshare')
         ax[0].set_xlabel('District, ranked by candidate 1 voteshare')
-    
-    fig.savefig(f'Analysis/Elections/{f}.pdf', bbox_inches='tight')
 
+    fig.savefig(f'/Analysis/Elections/{f}.pdf', bbox_inches='tight')
