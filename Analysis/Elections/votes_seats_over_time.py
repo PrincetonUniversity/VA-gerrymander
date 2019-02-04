53b46fc9a369f5298a18698d1e161b4d28433071
Will Adler <will@wtadler.com>
2019-02-04 14:37:18 -0500

    votes-seats over time, all ’16–’18 statewides

--- Analysis/Elections/votes_seats_over_time.py
+++ Analysis/Elections/votes_seats_over_time.py
@@ -9,6 +9,7 @@ import seaborn as sns
 sns.set()
 %config InlineBackend.figure_format = 'svg'
 import matplotlib.ticker as ticker
+import numpy as np
 
 maps = {'enacted': {'name': 'Enacted map',
                     'path': 'Maps/Enacted map/enacted.shp',
@@ -79,49 +80,16 @@ vs_en = np.array([table.loc[table['map']==en, race].mean() for race in races.key
 ss_en = np.array([sum(table.loc[table['map']==en, race]>.5)/100 for race in races.keys()])
 ss_sm = np.array([sum(table.loc[table['map']==sm, race]>.5)/100 for race in races.keys()])
 
-
 #%%
-# key_races = ['Clinton16', 'LtGov17_Fairfax', 'Sen18_Kaine']
-# xticklabels = ['2016 (Pres.)', '2017 (Lt. Gov.)', '2018 (US Sen.)']
-# note_keys = np.where([i in key_races for i in races.keys()])[0]
-# 
-# fig, ax = plt.subplots(figsize=(5,5))
-# ax.plot(vs_en[note_keys], color='blue', label='Dem. 2-party vote share')
-# ax.scatter(range(3), vs_en[note_keys], color='blue')
-# 
-# ax.plot(ss_en[note_keys], color='black', label='Dem. seat share, 2011 Enacted map')
-# ax.scatter(range(3), ss_en[note_keys], color='black')
-# 
-# ax.plot(ss_sm[note_keys], color='black', linestyle='--',  label='Dem. seat share, 2019 Special Master map')
-# ax.scatter(range(3), ss_sm[note_keys], color='black')
-# 
-# ax.legend(loc='lower left')
-# yticks = np.linspace(.35, .65, 7)
-# ax.set(xticks=range(3),
-#         xticklabels=xticklabels,
-#         yticks=yticks,
-#         yticklabels=[f'{int(100*i)}%' for i in yticks],
-#         ylim=[.31, .68],
-#         xlim=[-.5, 2.5])
-# 
-# ax.yaxis.set_minor_locator(ticker.MultipleLocator(.01))
-# ax.grid(b=True, which='major', color='w', linewidth=2, axis='y')
-# ax.grid(b=True, which='minor', color=(.99, .99, .99), linewidth=.5, axis='y')
-# ax.grid(b=True, which='major', color='w', linewidth=0, axis='x')
-# ax.axhline(.5, color=(.5, .5, .5), linewidth=2.5);
-# fig.savefig('dem_trend3_fairfax.png', dpi=300, bbox_inches='tight')
+# avg the 2017 races
+use_races = [['Clinton16'], ['Gov17_Northam', 'LtGov17_Fairfax', 'AtGen17_Herring'], ['Sen18_Kaine']]
 
-
-
-#%%
-races = [['Clinton16'], ['Gov17_Northam', 'LtGov17_Fairfax', 'AtGen17_Herring'], ['Sen18_Kaine']]
-
-vs_en = [np.mean([table.loc[table['map']==en, i].mean() for i in year]) for year in races]
-ss_en = np.array([np.mean([sum(table.loc[table['map']==en, i]>.5) for i in year]) for year in races])/100
-ss_sm = np.array([np.mean([sum(table.loc[table['map']==sm, i]>.5) for i in year]) for year in races])/100
+vs_en = [np.mean([table.loc[table['map']==en, i].mean() for i in year]) for year in use_races]
+ss_en = np.array([np.mean([sum(table.loc[table['map']==en, i]>.5) for i in year]) for year in use_races])/100
+ss_sm = np.array([np.mean([sum(table.loc[table['map']==sm, i]>.5) for i in year]) for year in use_races])/100
 
 xticklabels = ['2016', '2017', '2018']
-
+#%%
 fig, ax = plt.subplots(figsize=(5,5))
 ax.plot(vs_en, color='blue', label='Dem. 2-party vote share')
 ax.scatter(range(3), vs_en, color='blue')
@@ -134,20 +102,74 @@ ax.scatter(range(3), ss_sm, color='black')
 
 ax.legend(loc='lower left')
 yticks = np.linspace(.35, .65, 7)
+xlim=[-.5, 2.5]
+ylim=[.31, .68]
 ax.set(xticks=range(3),
         xticklabels=xticklabels,
         yticks=yticks,
         yticklabels=[f'{int(100*i)}%' for i in yticks],
-        ylim=[.31, .68],
-        xlim=[-.5, 2.5])
-# ax.xaxis.set_major_locator(ticker.MultipleLocator(.05))
-# ax.xaxis.set_minor_locator(ticker.MultipleLocator(.25))
-# 
-# ax.yaxis.set_major_locator(ticker.MultipleLocator(.05*N))
+        ylim=ylim,
+        xlim=xlim,
+        title='Democrats are gaining strength in Virginia. \nSo much strength, that they win overwhelmingly under either map.')
+
 ax.yaxis.set_minor_locator(ticker.MultipleLocator(.01))
 ax.grid(b=True, which='major', color='w', linewidth=2, axis='y')
 ax.grid(b=True, which='minor', color=(.99, .99, .99), linewidth=.5, axis='y')
 ax.grid(b=True, which='major', color='w', linewidth=0, axis='x')
 ax.axhline(.5, color=(.5, .5, .5), linewidth=2.5);
-fig.savefig('dem_trend3_2017avg.png', dpi=300, bbox_inches='tight')
+
+caption = '''
+
+
+The Democratic share of the vote in recent statewide elections
+(blue line) has been steadily increasing. Projected seat share
+under the 2011 Enacted map (solid black line) and the 2019 Special
+Master map (dashed black line) were obtained for each election
+by aggregating precinct-level results. Note that, in a single-member
+district system such as the House of Delegates, a discrepancy
+between vote share and seat share (also known as the “winners’
+bonus”) is expected. It is not at all unusual for the seat margin
+to be double that of the vote margin, as in the 2017 and 2018
+elections shown here. Electoral precinct data and maps were compiled
+by the Princeton Gerrymandering Project. Electoral data was used
+from 2016 Presidential, 2017 Governor, Lt. Governor, Atty. Gen.,
+and 2018 U.S. Senator races.'''
+
+ax.text(xlim[0], ylim[0], caption, ha='left', va='top', fontsize=9, color='gray')
+fig.savefig('Analysis/Elections/votes_seats_over_time.pdf', bbox_inches='tight')
+
+
+
+#%%
+xticklabels = list(races.keys())
+vs_en = [np.mean(table.loc[table['map']==en, race]) for race in xticklabels]
+ss_en = np.array([sum(np.array(table.loc[table['map']==en, race])>.5) for race in xticklabels])/100
+ss_sm = np.array([sum(np.array(table.loc[table['map']==sm, race])>.5) for race in xticklabels])/100
+
+
+
+fig, ax = plt.subplots(figsize=(5,5))
+ax.plot(vs_en, color='blue', label='Dem. 2-party vote share')
+
+ax.plot(ss_en, color='black', label='Dem. seat share, 2011 Enacted map')
+
+ax.plot(ss_sm, color='black', linestyle='--',  label='Dem. seat share, 2019 Special Master map')
+races.keys()
+
+ax.legend(loc='lower left')
+yticks = np.linspace(.35, .65, 7)
+xlim = [-.5, 7.5]
+ax.set(xticks=range(len(races)),
+        yticks=yticks,
+        yticklabels=[f'{int(100*i)}%' for i in yticks],
+        ylim=[.31, .68],
+        xlim=xlim)
+ax.set_xticklabels(xticklabels, rotation=45, ha='right')
+ax.yaxis.set_minor_locator(ticker.MultipleLocator(.01))
+# ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
+ax.grid(b=True, which='major', color='w', linewidth=2, axis='y')
+ax.grid(b=True, which='minor', color=(.99, .99, .99), linewidth=.5, axis='y')
+ax.grid(b=True, which='major', color='w', linewidth=1, axis='x')
+ax.axhline(.5, color=(.5, .5, .5), linewidth=2.5);
+fig.savefig('Analysis/Elections/votes_seats_over_time_all_races.pdf', bbox_inches='tight')
 
